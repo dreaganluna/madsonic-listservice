a8a678503c38ec7cc9447b1421e2bf0a0bd8ceb2
Mikaël Spruyt <draeganlunacrivassan@gmail.com>
2017-09-17 11:51:43 +0200

    cleanup
    removed unused packages, removed log prefix from log files, removed config details from project and added a config template json file, added config entries for lastfmservice and songservice

--- node_logging/logger.js
+++ node_logging/logger.js
@@ -4,7 +4,7 @@ var winston = require('winston');
 	var logObject =  new (winston.Logger)({
     transports: [
       new (winston.transports.Console)(),
-      new (winston.transports.File)({ filename: '/nodelogs/log'+ logName +'.log', 'level': 'verbose', 'json': false ,handleExceptions: true,    humanReadableUnhandledException: true, maxsize: 5000000, maxFiles: 5, tailable: true})
+      new (winston.transports.File)({ filename: '/nodelogs/'+ logName +'.log', 'level': 'verbose', 'json': false ,handleExceptions: true,    humanReadableUnhandledException: true, maxsize: 5000000, maxFiles: 5, tailable: true})
     ]
   });
 
